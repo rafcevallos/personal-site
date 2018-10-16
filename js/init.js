f706083e90155563834d91b8344cf516b167b1b3
rafcevallos <rafael.cevallos81@gmail.com>
2018-10-16 00:17:04 -0500

    first commit

---
+++ js/init.js
@@ -0,0 +1,13 @@
+(function ($) {
+  $(function () {
+
+    $('.sidenav').sidenav();
+    $('.parallax').parallax();
+    $('.carousel.carousel-slider').carousel({
+      fullWidth: true
+    });
+    $('.materialboxed').materialbox();
+
+  }); // end of document ready
+
+})(jQuery); // end of jQuery name space
